85248535e94d16fba2f274dc6f0ebcfd8363d0a1
dev-rome <haynesjerome@gmail.com>
2023-05-02 18:08:03 -0400

    added component to display student email, compnay, skill, and average, also added memo

--- src/components/students/StudentInfoList.jsx
+++ src/components/students/StudentInfoList.jsx
@@ -1,12 +1,14 @@
-function StudentInfoList({ student, calculateAverage }) {
-    return (
-      <ul className="font-raleway ml-3 md:ml-6 mb-3">
-        <li>Email: {student.email}</li>
-        <li>Company: {student.company}</li>
-        <li>Skill: {student.skill}</li>
-        <li>Average: {calculateAverage()}%</li>
-      </ul>
-    );
-  }
+import { memo } from "react";
 
-  export default StudentInfoList;
+const StudentInfoList = ({ student, calculateAverage }) => {
+  return (
+    <ul className="font-raleway ml-3 md:ml-6 mb-3">
+      <li>Email: {student.email}</li>
+      <li>Company: {student.company}</li>
+      <li>Skill: {student.skill}</li>
+      <li>Average: {calculateAverage()}%</li>
+    </ul>
+  );
+};
+
+export default memo(StudentInfoList);
